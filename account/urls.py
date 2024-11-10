66a5c32cdbbfe0e48b8ec70e2b71ae511cfb9441
Dontgoingforyou <mishael000@yandex.ru>
2024-11-10 04:19:33 +0300

    created a login and logout

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from account.apps import AccountConfig
-from .views import user_login
-
-app_name = AccountConfig.name
+from .views import dashboard
 
 urlpatterns = [
-    path('login/', user_login, name='login')
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
+    path('', dashboard, name='dashboard'),
 ]
