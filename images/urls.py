349abfa9031a9bd944ac04ae80eaa042233d5dd9
Dontgoingforyou <mishael000@yandex.ru>
2024-11-13 12:07:33 +0300

    created a view of detailed information about images

--- images/urls.py
+++ images/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from images.apps import ImagesConfig
-from images.views import image_create
+from images.views import image_create, image_detail
 
 app_name = ImagesConfig.name
 
 urlpatterns = [
     path('create/', image_create, name='create'),
+    path('detail/<int:id>/<slug:slug>/', image_detail, name='detail'),
 ]
