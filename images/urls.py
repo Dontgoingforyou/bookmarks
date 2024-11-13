2e0bf721c66ab6099a18c4653e2bf603785455c5
Dontgoingforyou <mishael000@yandex.ru>
2024-11-13 16:11:21 +0300

    implemented endless page-by-page scrolling

--- images/urls.py
+++ images/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from images.apps import ImagesConfig
-from images.views import image_create, image_detail, image_like
+from images.views import image_create, image_detail, image_like, image_list
 
 app_name = ImagesConfig.name
 
 urlpatterns = [
     path('create/', image_create, name='create'),
     path('detail/<int:id>/<slug:slug>/', image_detail, name='detail'),
-
+    path('', image_list, name='list'),
     path('like/', image_like, name='like'),
 ]
